37817c779a9c8216e34ff09aa5f5eedabf349651
Michelle David <rachaelholy@gmail.com>
2022-05-09 00:20:25 +0100

    Updated to enable users connect wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useAddress, useMetamask } from '@thirdweb-dev/react';
+
+
 const App = () => {
+  // Hook from thirdweb
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("Address..", address);
+
+  // if the user has not connected their wallet
+  if(!address){
+    return (
+      <div className="landing">
+        <h1>Hi! Welcome to Meme Dao👋 </h1>
+        <button onClick={connectWithMetamask} className="btn-hero">Connect wallet</button>
+      </div>
+    );
+  }
+  //if the user's wallet is already connected 
   return (
     <div className="landing">
       <h1>Hi!👋 Meme DAO👽</h1>
+      <p>Wallet connected!</p>
     </div>
   );
 };
