4684364074b84a9c97823b2683770cca536f0b4e
Michelle David <rachaelholy@gmail.com>
2022-05-16 22:38:20 +0100

    Updated airdrop function

--- scripts/7-airdrop-token.js
+++ scripts/7-airdrop-token.js
@@ -8,12 +8,29 @@ const token = await sdk.getToken("0x218D3686d4d45E5ecAaAb8b451a1cF13A93329Ec");
 (async () => {
     try {
       /// grap addresses of people who own our membership NFT, with the tokenId of 0
-      const membersAddress = await editionDrop.history.getAllClaimerAddresses("0");
+      const membersAddress = await editionDrop.history.getAllClaimerAddresses(0);
 
       if (membersAddress.length === 0) {
           console.log("No NFT has been claimed yet, get your friends to claim your NFT");
           process.exit(0);
       }
+     ///loop through the array of addresses 
+      const airDropAddress = await membersAddress.map(address => {
+          //pick a random number from 1000 to 100000
+         const randomAmount = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
+         console.log("airdropping ", randomAmount, "with", address, "address"); 
+
+         //Airdrop target
+         const airdropTarget = {
+             toAddress: address,
+             amount: randomAmount,
+         };
+         return airdropTarget;
+      });
+
+      console.log("🌈 Started airdropping...");
+      await token.transferBatch(airDropAddress);
+      console.log("✅ Successfully airdropped token to all the holders of the NFT");
 
     } catch (err) {
         console.log("Failed to airdrop token", err);
