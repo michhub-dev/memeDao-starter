881ecc228f1c33557fe61aa401eb4a6946099efd
Michelle David <rachaelholy@gmail.com>
2022-05-19 13:24:46 +0100

    Fix bug

--- scripts/8-deploy-vote.js
+++ scripts/8-deploy-vote.js
@@ -3,11 +3,12 @@ import sdk from "./1-initialize-sdk.js";
 
 (async () =>{
     try {
+        ///this will deploy a brand new voting contract 
      const voteGovContract = await sdk.deployer.deployVote({
          //governanace contract name 
          name: "Meme Governance Dao",
          // location of the governance token, the ERC-20 token 
-         voting_token_location: "0x218D3686d4d45E5ecAaAb8b451a1cF13A93329Ec",
+         voting_token_address: "0x218D3686d4d45E5ecAaAb8b451a1cF13A93329Ec",
          // when members can start voting 
          voting_delay_in_blocks: 0,
          //how long members have to vote on a proposal when it's created, 1 day = 6570 blocks
@@ -16,10 +17,10 @@ import sdk from "./1-initialize-sdk.js";
       // the proposal to be valid after the time for the proposal has ended
          voting_quorum_fraction: 0,
          //minimum # of token a user needs to be allowed to create a proposal 
-         proposal_token_threshold: 0
+         proposal_token_threshold: 0,
      });
-     console.log("Successfully deployed a contract, address", voteGovContract);
-    } catch (error) {
-        console.log("Failed to deploy the vote com", error);
+     console.log("Successfully deployed vote contract, address", voteGovContract);
+    } catch (err) {
+        console.error("Failed to deploy the vote contract", err);
     }
 })();
